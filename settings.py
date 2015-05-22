c278ba4986a438c49805fd46a320134ec6b3fe0f
laserman <mherweg@das-labor.org>
2015-05-22 08:56:40 +0200

    moved db connection settings to settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# settings.py
+
+#db_params = {"host":"eclipse.optiputer.net", "database":"landcover", "user":"mherweg"}
+db_params = {"host":"localhost", "database":"landcover", "user":"mherweg"}
+infile = open("apt.dat", 'r')
+
+
+#EOF
