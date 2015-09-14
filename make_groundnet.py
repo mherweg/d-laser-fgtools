65e6d3b5ba6b39938d5f6d2752dcdf5b5ed46cf1
laserman <mherweg@das-labor.org>
2015-09-14 22:30:39 +0200

    make_groundnet.py - possibly a working version, needs testing

--- make_groundnet.py
+++ make_groundnet.py
@@ -5,18 +5,40 @@
 # 
 
 
-import sys
+import sys, numpy
 
 
-filename="EDLM.dat"
-airport="EDLM"
+#filename="EDLM.dat"
+#airport="EDLM"
+
+airport = sys.argv[2]
+filename = sys.argv[1]
 
 infile = open(filename, 'r')
 
-#airport = sys.argv[2]
-
 found = False
 
+def convert_lat(lat):
+    if lat < 0:
+		    lat2 = "S%d %f"%(int(abs(lat)), (abs(lat) - int(abs(lat)) )* 60)
+    else:
+		    lat2 = "N%d %f"%(int(abs(lat)), (abs(lat) - int(abs(lat)) )* 60)
+    return(lat2)
+    
+def convert_lon(lon):
+    if lon < 0:
+		    lon2 = "W%d %f"%(int(abs(lon)), (abs(lon) - int(abs(lon)) )* 60)
+    else:
+		    lon2 = "E%d %f"%(int(abs(lon)), (abs(lon) - int(abs(lon)) )* 60)
+    return (lon2)
+    
+def calculate_distance(p,n):    
+    a = numpy.array((p[0] ,p[1], 0))
+    b = numpy.array((n[1], n[2], 0))
+    dist = numpy.linalg.norm(a-b)
+    return dist
+    
+    
 for line in infile:
             line = line.strip()
             # 1 for airports, 16 for seaports
@@ -70,15 +92,7 @@ for line in infile:
                             
             # If found, convert values
             if lat != -555:
-				if lat < 0:
-						lat2 = "S%d %f"%(int(abs(lat)), (abs(lat) - int(abs(lat)) )* 60)
-				else:
-						lat2 = "N%d %f"%(int(abs(lat)), (abs(lat) - int(abs(lat)) )* 60)
-				if lon < 0:
-						lon2 = "W%d %f"%(int(abs(lon)), (abs(lon) - int(abs(lon)) )* 60)
-				else:
-						lon2 = "E%d %f"%(int(abs(lon)), (abs(lon) - int(abs(lon)) )* 60)
-				parkings.append((lat2, lon2, heading, name),)                
+		parkings.append([lat, lon, heading, name, -1])                
 
             # taxinode
             #                                      1              2            3      4       5   
@@ -92,15 +106,7 @@ for line in infile:
 		nodetype = result.group(3)
 		nodeid = result.group(4)
 		nodename = result.group(5).replace(' ', '_')
-		if lat < 0:
-			lat2 = "S%d %f"%(int(abs(lat)), (abs(lat) - int(abs(lat)) )* 60)
-		else:
-			lat2 = "N%d %f"%(int(abs(lat)), (abs(lat) - int(abs(lat)) )* 60)
-		if lon < 0:
-			lon2 = "W%d %f"%(int(abs(lon)), (abs(lon) - int(abs(lon)) )* 60)
-		else:
-			lon2 = "E%d %f"%(int(abs(lon)), (abs(lon) - int(abs(lon)) )* 60)
-		taxinodes.append((nodeid, lat2, lon2),)
+		taxinodes.append((nodeid, lat, lon),)
 #        N51 38.894557 E7 9.686969 both 1 _start
 		#print nodeid , lat2, lon2, nodetype
 		nodeid_int = int(result.group(4))
@@ -123,16 +129,34 @@ for line in infile:
 		onetwo = result.group(3)
 		rwtw = result.group(4)
 		name = result.group(5)
-		taxiedges.append((n1, n2, name ),)
+		taxiedges.append((n1, n2, "0", name ),)
            
-
 infile.close()
 
-# i = 0
-print('<?xml version="1.0"?>\n<groundnet>\n   <parkingList>')
+i=900
 for p in parkings:
     i = i + 1
-    print('        <Parking index="%d" type="gate" name="%s" lat="%s" lon="%s" heading="%f" />'%(i, p[3], p[0], p[1], p[2]))
+    p[4]=i
+    mindist=1.0
+    bestnode=-1
+    for n in taxinodes:
+	    dist = calculate_distance(p,n)
+	    if dist < mindist:
+		mindist=dist
+		#print "new mindist" , dist
+		bestnode = n
+    #print p , bestnode
+    if bestnode != -1:
+	taxiedges.append((p[4], bestnode[0], "1", "toGate" ),)
+    else:
+	print "no node for ", p
+    
+
+
+print('<?xml version="1.0"?>\n<groundnet>\n   <parkingList>')
+for p in parkings:
+
+    print('        <Parking index="%d" type="gate" name="%s" lat="%s" lon="%s" heading="%6.2f" />'%(p[4], p[3], convert_lat(p[0]), convert_lon(p[1]), p[2]))
             
 print("   </parkingList>")
 
@@ -140,17 +164,18 @@ print "   <TaxiNodes>"
 for n in taxinodes:
     nodeid_int=int(n[0])
 	# <node index="552" lat="N52 18.513" lon="E04 46.375" isOnRunway="0" holdPointType="PushBack" />
-    print('        <node index="%d" lat="%s" lon="%s" isOnRunway="0" holdPointType="none" />'%(nodeid_int, n[1], n[2]))
-    i = i + 1
+    print('        <node index="%d" lat="%s" lon="%s" isOnRunway="0" holdPointType="none" />'%(nodeid_int, convert_lat(n[1]), convert_lon(n[2])))
+
 print "   </TaxiNodes>"
 
 
 
 print "   <TaxiWaySegments>"
- #<TaxiWaySegments>
+
  #   <arc begin="0" end="315" isPushBackRoute="1" name="Route" />
 for e in taxiedges:
-    print('        <arc begin="%s" end="%s" isPushBackRoute="0" name="%s"  />'%(e[0], e[1], e[2]))
+    print('        <arc begin="%s" end="%s" isPushBackRoute="%s" name="%s"  />'%(e[0], e[1], e[2], e[3]))
+
 print "   </TaxiWaySegments>"
 print "\n</groundnet>"
 
