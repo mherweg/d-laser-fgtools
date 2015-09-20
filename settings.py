6460be3cd325e834530daae7c64b3890e69af2ec
Martin Herweg <mherweg@sphere.telascience.org>
2015-09-20 06:46:59 -0700

    foo bar
    settings

--- settings.py
+++ settings.py
@@ -3,8 +3,8 @@
 #
 # settings.py
 
-#db_params = {"host":"eclipse.optiputer.net", "database":"landcover", "user":"mherweg"}
-db_params = {"host":"localhost", "database":"landcover", "user":"mherweg"}
+db_params = {"host":"eclipse.optiputer.net", "database":"landcover", "user":"mherweg"}
+#db_params = {"host":"localhost", "database":"landcover", "user":"mherweg"}
 infile = open("apt.dat", 'r')
 
 
